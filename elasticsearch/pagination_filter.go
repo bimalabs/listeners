a1a558a590ce80bf8d2a32519c9f44667efcb507
Muhamad Surya Iksanudin <surya.kejawen@gmail.com>
2022-07-10 11:59:51 +0700

    split listeners to own package

---
+++ elasticsearch/pagination_filter.go
@@ -0,0 +1,40 @@
+package elasticsearch
+
+import (
+	"strings"
+
+	"github.com/bimalabs/framework/v4/events"
+	"github.com/olivere/elastic/v7"
+)
+
+type PaginationFilterElasticsearch struct {
+}
+
+func (p *PaginationFilterElasticsearch) Handle(event interface{}) interface{} {
+	e, ok := event.(*events.ElasticsearchPagination)
+	if !ok {
+		return event
+	}
+
+	var wildCard strings.Builder
+	for _, v := range e.Filters {
+		wildCard.Reset()
+		wildCard.WriteString("*")
+		wildCard.WriteString(v.Value)
+		wildCard.WriteString("*")
+
+		q := elastic.NewWildcardQuery(v.Field, wildCard.String())
+		q.Boost(1.0)
+		e.Query.Must(q)
+	}
+
+	return e
+}
+
+func (p *PaginationFilterElasticsearch) Listen() string {
+	return events.PaginationEvent.String()
+}
+
+func (p *PaginationFilterElasticsearch) Priority() int {
+	return 255
+}
